58ae1351780cbd00c2a7ce8322a0d49901391b3f
Brian Rinaldi <brian.rinaldi@gmail.com>
2021-12-08 13:29:23 -0500

    Update htmlRewrite.js

--- functions/htmlRewrite.js
+++ functions/htmlRewrite.js
@@ -5,16 +5,14 @@ class ElementHandler {
 }
 const rewriter = new HTMLRewriter().on("h1", new ElementHandler());
 
-export default {
-  async fetch() {
-    const res = await fetch(request);
-    const contentType = res.headers.get("Content-Type");
-    // If the response is HTML, it can be transformed with
-    // HTMLRewriter -- otherwise, it should pass through
-    if (contentType?.startsWith("text/html")) {
-      return rewriter.transform(res);
-    } else {
-      return res;
-    }
-  },
-};
+export async function onRequest(context) {
+  const res = await fetch(context.request);
+  const contentType = res.headers.get("Content-Type");
+  // If the response is HTML, it can be transformed with
+  // HTMLRewriter -- otherwise, it should pass through
+  if (contentType?.startsWith("text/html")) {
+    return rewriter.transform(res);
+  } else {
+    return res;
+  }
+}
